7ddd1401114043881993c4bb928a7c5be5c297a3
x00082952 <x00082952@gmail.com>
2016-12-05 23:11:31 +0000

    Starting Lab2

---
+++ src/app/click-me/click-me.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-click-me',
+  templateUrl: './click-me.component.html',
+  styleUrls: ['./click-me.component.css']
+})
+export class ClickMeComponent implements OnInit {
+
+  // Declare and initialise a String
+  clickMessage: string = "";
+
+  constructor() { }
+
+  onClickMe() {
+    //Set the message
+    this.clickMessage = "You clicked the button!";
+  }
+
+  ngOnInit() {
+  }
+
+}
